08d319a1be5a68147937acbbfef1881aea8799b6
Krishnakumar <Krishnakumarsdckap@gmail.com>
2026-09-10 15:51:38 +0530

    changed db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg")
 require("dotenv").config()
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST||"aws-0-ap-northeast-2.pooler.supabase.com",
+  port: process.env.DB_PORT||5432,
+  user: process.env.DB_USER||"postgres.ghdbvfxksdvahhasgxny",
+  password: process.env.DB_PASSWORD||"Online_Food_Ordering_System",
+  database: process.env.DB_DATABASE||"postgres",
   ssl: { rejectUnauthorized: false }, // Supabase requires SSL
 })
 
